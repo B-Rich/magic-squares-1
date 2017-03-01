609ed411d48319141afeff351e4d67779c5299d3
William Sutton <William Sutton>
2017-02-28 19:01:16 -0500

    initial work

---
+++ main.py
@@ -0,0 +1,140 @@
+import time
+import random
+import os, sys
+
+t0 = time.time()
+f = open('dictionary.txt', 'r')
+lines = f.readlines()
+f.close()
+t1 = time.time()
+
+print 'time to readlines: ', (str( t1 - t0)[:5])
+print 'number of lines: ', len(lines)
+print '\n'
+for i in range(5):
+    r = random.randint(0,len(lines))
+    print 'word #', str(r), ' : ', str(lines[r])
+
+
+t0 = time.time()
+words = []
+for line in lines:
+    strip_line = line.rstrip("\n")
+    words.append(strip_line)
+t1= time.time()
+
+print 'len of words: ', str(len(words))
+print 'time to create list : ', str(t1-t0)[:5]
+
+print "FALSE: lines[0]=='AA': ", str(lines[0]=='AA')
+print "TRUE: lines[0]=='AA\n': ", str(lines[0]=='AA\n')
+print "TRUE: words[0]=='AA': ", str(words[0]=='AA')
+
+def n_letter_words(n, input, **kwargs):
+    return filter(lambda w: len(w) == n, input)
+
+for _n in range(1,20):
+    
+    n_letters = n_letter_words(_n, words)
+    print "n=",str(_n), ": ", str(len(n_letters))    
+    
+    if len(n_letters) > 0:
+        out = n_letters[:min(4, len(n_letters))]
+        print ("\n").join(out)
+
+def search_me(_w, words):
+    return filter(lambda w: w ==_w, words)
+
+
+for this_word in ["AA", "ITALIAN", "ENGLISH", "PHALLIC", "ABACUS", "ITALY", "ENGLAND"]:
+    print this_word, " - ", str(search_me(this_word, words))
+
+
+print 'done'
+
+
+def search_word(N,**kwargs):
+    r = random.randint()
+    return 
+
+def satisy(lwords,inds):
+    
+    for ind in inds:
+        if 
+
+    return True
+
+good = ["WILL", "ICEY", "LEAN", "LYNX"]
+
+good = ["WILL", "ICEY", "LEAN", "LYNX"]
+print satisfy(good, range(4))
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ pp.py
@@ -0,0 +1,8 @@
+ww = "sfdkjf\nfdfdf\nsdsdsdsd\n"
+print ww
+
+q = ww.split("\n")
+print q
+
+qq = ww.rstrip()
+print qq
